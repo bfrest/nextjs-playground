35348ba6dd929d04a638e372bbeec8388f94cbef
Brendon <freston75@gmail.com>
2022-08-16 21:04:59 -0600

    added some styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import Nav from '../components/Nav';
 import '../styles/globals.css';
 
--- pages/assets.js
+++ pages/assets.js
@@ -2,6 +2,15 @@ import styled from 'styled-components';
 import testData from '../testData';
 
 const AssetStyle = styled.div`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   width: 100%;
   height: 100%;
   padding: 20px;
@@ -10,14 +19,28 @@ const AssetStyle = styled.div`
   grid-template-columns: 1fr 1fr;
 
   div {
-    color: white;
-    background-color: #040404;
-    max-width: 30%;
-    margin: 1rem 0;
+    background: white;
+    border: 1px solid #ededed;
+    box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.09);
+    position: relative;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    border: 2px solid black;
+    width: 450px;
+    margin: 30px 0;
+
+    img {
+      width: 100%;
+      height: 200px;
+      object-fit: cover;
+    }
+
+    p {
+      line-height: 2;
+      font-weight: 300;
+      flex-grow: 1;
+      padding: 0 3rem;
+      font-size: 1.5rem;
+    }
 
     img {
       height: 300px;
@@ -33,7 +56,7 @@ export default function assets() {
         return (
           <div key={product.id}>
             <img src={product.images[0]} />
-            <p>{product.brand}</p>
+            <p>{product.brand.toUpperCase()}</p>
           </div>
         );
       })}
